48faedc5bdbe18cefbe536d734b073533071a92b
Pietro Mascolo <iz4vve@gmail.com>
2021-03-30 21:02:46 +0100

    ADDS nine2ten

--- cmd/nine2ten/main.go
+++ cmd/nine2ten/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/iz4vve/go-icd10"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: nine2ten <code1> <code2> <code3> ...")
+		os.Exit(1)
+	}
+
+	codes := os.Args[1:]
+	ret, err := icd10.NineToTen(codes)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(strings.Join(ret, " "))
 }
--- cmd/ten2nine/main.go
+++ cmd/ten2nine/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/iz4vve/go-icd10"
 )
 
 func main() {
-	fmt.Println("10 2 9!")
-	fmt.Println(icd10.TenToNine([]string{"A000"}))
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: ten2nine <code1> <code2> <code3> ...")
+		os.Exit(1)
+	}
+
+	codes := os.Args[1:]
+	ret, err := icd10.TenToNine(codes)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(strings.Join(ret, " "))
 }
